41a3d06025b8cf6bea03709c9bbd58e1988761b0
V_Tsabovskiy <pankiahoy@gmail.com>
2021-02-24 16:06:03 +0300

    1.1.4 added

---
+++ Task 1/1.1/1.1.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1._1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int m = 1; m <= n; m++)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    String str = "";
+                    for (int j = 0; j < n - 1 - i; j++)
+                    {
+                        str += " ";
+                    }
+                    for (int j = 0; j < 2 * i + 1; j++)
+                    {
+                        str += "*";
+                    }
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
