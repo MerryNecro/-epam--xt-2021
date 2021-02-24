bc1907b612da3b0ea7ecb4e249446a106d45faf5
V_Tsabovskiy <pankiahoy@gmail.com>
2021-02-24 14:04:25 +0300

    1.1.1 added

---
+++ Task 1/1.1/1.1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину первой стороны: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину второй стороны: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a <= 0 || b <= 0) { Console.WriteLine("Неверное значение одной из сторон"); }
+            else { Console.WriteLine("Площадь прямоугольника равна: " + a*b); }
+        }
+    }
+}
