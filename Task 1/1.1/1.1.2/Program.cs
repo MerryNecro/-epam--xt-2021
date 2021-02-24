f361853022626edb15df26dea75a7e4ee634e2a6
V_Tsabovskiy <pankiahoy@gmail.com>
2021-02-24 14:28:41 +0300

    1.1.2 added

---
+++ Task 1/1.1/1.1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N: ");
+            int n = int.Parse(Console.ReadLine());
+            String str ="";
+            for(int i = 0; i < n; i++)
+            {
+                str += "*";
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
