10a8c9b4a31a226fecfb317173e7a013f503f6fb
V_Tsabovskiy <pankiahoy@gmail.com>
2021-03-25 14:39:59 +0300

    Task 2.2 added

---
+++ Task 2/2.2/Objects.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2
+{
+    public abstract class Objects
+    {
+        public char symbol { protected set; get; }
+        public int x { protected set; get; }
+        public int y { protected set; get; }
+        public bool availableToGo { protected set; get; }
+
+        public Objects(char symbol, int x, int y, bool availableToGo)
+        {
+            this.symbol = symbol;
+            this.x = x;
+            this.y = y;
+            this.availableToGo = availableToGo;
+        }
+
+        public abstract void Print();
+    }
+}
---
+++ Task 2/2.2/Static.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2
+{
+    public abstract class Static : Objects
+    {
+        public Static(char symbol, int x, int y, bool availableToGo) : base(symbol, x, y, availableToGo) { }
+    }
+
+    public class Empty : Static
+    {
+        public Empty(int x, int y) : base(' ', x, y, true) { }
+
+        public override void Print()
+        {
+            Console.Write(symbol);
+        }
+    }
+
+
+    public abstract class Barrier : Static
+    {
+        public Barrier(char symbol, int x, int y) : base(symbol, x, y, false) { }
+
+        public override void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(symbol);
+        }
+
+        enum Barriers { };
+    }
+
+    public abstract class Bonus : Static
+    {
+        public Bonus(char symbol, int x, int y) : base(symbol, x, y, true) { }
+
+        public abstract void Append(Moveable moveable);
+
+        public override void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(symbol);
+        }
+    }
+}
