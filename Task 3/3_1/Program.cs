d542665422376d5de4e5cb98bdf9806f340b881b
V_Tsabovskiy <pankiahoy@gmail.com>
2021-04-15 17:08:48 +0400

    Added task 3_1

---
+++ Task 3/3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeakestLink a = new WeakestLink();
+            //Console.WriteLine( a.GetN());
+        }
+    }
+}
---
+++ Task 3/3_1/WeakestLink.cs
@@ -0,0 +1,86 @@
+﻿using _3_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_1
+{
+    class WeakestLink
+    {
+        private CycledDynamicArray<int> Humans;
+
+        private int N;
+        private int NumberOfHumanToDelete;
+
+        public WeakestLink() {
+            N = GetN();
+            NumberOfHumanToDelete = GetNumberOfHumanToDelete();
+            Humans = new CycledDynamicArray<int>(N);
+            for(int i = 0; i < N; i++)
+            {
+                
+            }
+            Kick();
+        }
+
+
+        private void Kick() 
+        {
+
+
+        }
+
+
+        private int GetN()
+        {
+            int result;
+            Console.Write("Enter N: ");
+            do
+            {
+                if(int.TryParse(Console.ReadLine(), out result))
+                {
+                    if (result <= 0)
+                    {
+                        Console.WriteLine("Number of people can't be < or = 0");
+                    }
+                    else
+                    {
+                        return result;
+                    }
+                }
+                Console.Write("Something is wrong. Try again: ");
+
+            } while (true);
+        }
+
+
+        private int GetNumberOfHumanToDelete()
+        {
+            int result;
+            Console.Write("Enter number of person to kick: ");
+            do
+            {
+                if (int.TryParse(Console.ReadLine(), out result))
+                {
+                    if (result <= 0 || result > N)
+                    {
+                        Console.WriteLine("Number of people can't be <= 0 or > N");
+                    }
+                    else
+                    {
+                        return result;
+                    }
+                }
+                Console.Write("Something is wrong. Try again: ");
+
+            } while (true);
+        }
+
+
+
+    }
+}
+
+
